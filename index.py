c0370d44a6717632d1168715245f95a081553c05
QLabs <55121845+Quantalabs@users.noreply.github.com>
2020-12-14 11:09:10 -0800

    Added requirements file and fixed country data bug.

--- index.py
+++ index.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands
 import covid19cases as covid
+from covid.api import CovId19Data
+
+api = CovId19Data(force=False)
 
 TOKEN = open("token.txt","r").readline()
 prefix = 'info '
@@ -13,7 +16,7 @@ async def help(ctx):
     embed = discord.Embed(
         colour = discord.Colour.blue())
     embed.set_author(name='Commands')
-    embed.add_field(name='cases', value='Returns general data about global cases, or add the name of a country or continent after to get data for it. NOTE: Not all countries will have data, so it might output an error.', inline=False)
+    embed.add_field(name='cases', value='Returns general data about global cases, or add the name of a country after to get data for it. NOTE: Not all countries will have data, so it might output an error.', inline=False)
     embed.add_field(name='credits', value='Returns data source and APIs used in the bot')
     await ctx.send(embed=embed)
 
@@ -44,44 +47,26 @@ async def cases(ctx, region=None):
 
         await ctx.send(embed=embed)
     else:
-        if region == "Asia" or region == "Europe" or region == "North America" or region == "South America" or region == "Africa" or region == "Australia":
-            cases = covid.get_continent_cases(region)
-            
-            embed = discord.Embed(
-                colour = discord.Colour.red()
-            )
-            embed.set_author(name=region + ' Statistics')
-            embed.add_field(name='Cases', value=cases["TotalCases"], inline=False)
-            embed.add_field(name="Deaths", value=cases["TotalDeaths"], inline=False)
-            embed.add_field(name="Recovered", value=cases["TotalRecovered"], inline=False)
-            embed.add_field(name="Active Cases", value=cases["ActiveCases"], inline=False)
-            embed.add_field(name="Critical", value=cases["Critical"], inline=False)
-            embed.add_field(name="Last Updated", value=cases["LastUpdated"], inline=False)
+        data = api.filter_by_country("ireland")
 
-            await ctx.send(embed=embed)
-        else:    
-            cases = covid.get_country_cases(region)
-            
-            embed = discord.Embed(
-                colour = discord.Colour.red()
-            )
-            embed.set_author(name=region + ' Statistics')
-            embed.add_field(name='Cases', value=cases["TotalCases"], inline=False)
-            embed.add_field(name="Deaths", value=cases["TotalDeaths"], inline=False)
-            embed.add_field(name="Recovered", value=cases["TotalRecovered"], inline=False)
-            embed.add_field(name="Active Cases", value=cases["ActiveCases"], inline=False)
-            embed.add_field(name="Critical", value=cases["Critical"], inline=False)
-            embed.add_field(name="Cases Per Million", value=cases["CasesPerOneMillion"], inline=False)
-            embed.add_field(name="Deaths Per Million", value=cases["DeathsPerOneMillion"], inline=False)
-            embed.add_field(name="Last Updated", value=cases["LastUpdated"], inline=False)
+        embed = discord.Embed(
+            colour = discord.Colour.blue()
+        )
+        embed.set_author(name='COVID-19')
+        embed.title = region + ' Statistics'
+        embed.add_field(name='Cases', value=data['confirmed'], inline=False)
+        embed.add_field(name='Recovered', value=data['recovered'], inline=False)
+        embed.add_field(name='Deaths', value=data['deaths'], inline=False)
+        embed.add_field(name='Last Updated', value=data['last_updated'], inline=False)
 
-            await ctx.send(embed=embed)
+        await ctx.send(embed=embed)
 
 @client.command()
 async def credits(ctx):
-    api = 'https://pypi.org/project/COVID-19-Cases/ - COVID-19-Cases was used to take data from the source and output \n'
-    source = 'https://www.worldometers.info/coronavirus/ - Info from here'
+    api = 'https://pypi.org/project/COVID-19-Cases/ - COVID-19-Cases was used to take data from the source and output stats. \n'
+    api2 = 'https://pypi.org/project/covid-data-api/ - The _COVID Data API_ was used to take data from John Hopkins and output stats.'
+    source = 'https://www.worldometers.info/coronavirus/ - Info from here \n'
 
-    await ctx.send(api + source)
+    await ctx.send(api + source + api2)
 
 client.run(TOKEN)
